0bfd6eb802a3eecebf210901fc82faadfd80cae3
Abey Thomas <abey.thomas@hashedin.com>
2021-03-03 14:21:49 +0530

    reset git

--- src/Card.java
+++ src/Card.java
@@ -1,45 +1,42 @@
 public class Card {
-    Suites suite;
-    Ranks rank;
-    int faceValue;
-    boolean isExposed;
+	Suites suite;
+	Ranks rank;
+	int faceValue;
+	boolean isExposed;
 
-    public Card(Suites suite, Ranks rank, int faceValue) {
-        this.suite = suite;
-        this.rank = rank;
-        this.faceValue = faceValue;
-        this.isExposed = false;
-    }
+	public Card(Suites suite, Ranks rank, int faceValue) {
+		this.suite = suite;
+		this.rank = rank;
+		this.faceValue = faceValue;
+		this.isExposed = false;
+	}
 
-    public Ranks getRank() {
-        return rank;
-    }
+	public Ranks getRank() {
+		return rank;
+	}
 
-    public Suites getSuite() {
-        return suite;
-    }
+	public Suites getSuite() {
+		return suite;
+	}
 
-    public int getFaceValue() {
-        return faceValue;
-    }
+	public int getFaceValue() {
+		return faceValue;
+	}
 
-    public void changeFaceValue(int faceValue) {
-        this.faceValue = faceValue;
-    }
+	public void changeFaceValue(int faceValue) {
+		this.faceValue = faceValue;
+	}
 
-    public void revealCard() {
-        this.isExposed = true;
-    }
+	public void revealCard() {
+		this.isExposed = true;
+	}
 
-    public boolean checkIfExposed() {
-        return isExposed;
-    }
+	public boolean checkIfExposed() {
+		return isExposed;
+	}
 
-    @Override
-    public String toString() {
-        return "Card{" +
-                "suite=" + suite +
-                ", rank=" + rank +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "Selected Card{" + "suite=" + suite + ", rank=" + rank + '}';
+	}
 }
